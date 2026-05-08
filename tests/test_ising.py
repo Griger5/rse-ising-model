fe9d0e895c5893d555dbdf86107a01748974d797
Griger5 <griger04@gmail.com>
2026-05-08 18:35:28 +0200

    update tests

--- tests/test_ising.py
+++ tests/test_ising.py
@@ -25,21 +25,6 @@ def test_run_ising_model_preserves_spin_values():
 
     assert np.all(np.logical_or(result == -1, result == 1))
 
-def test_run_ising_model_reproducible_with_seed():
-    np.random.seed(0)
-
-    grid1 = init_random_grid(20, 20)
-
-    result1 = run_ising_model(grid1.copy(), temperature=2.0, steps=10000)
-
-    np.random.seed(0)
-
-    grid2 = init_random_grid(20, 20)
-
-    result2 = run_ising_model(grid2.copy(), temperature=2.0, steps=10000)
-
-    assert np.array_equal(result1, result2)
-
 def test_low_temperature_increases_order():
     np.random.seed(0)
 
